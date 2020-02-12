19d0820976c3a9c5f4db58813ba1e7db9658c4e5
Mickael <mickael.merran@hotmail.com>
2020-02-12 15:11:52 +0100

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="centre">
+        <br>
+        <h1>Liste des films Batman</h1>
+        <br>
+        <div 
+             v-for="film in films"
+             :key="film.imdbID"
+             class='liste'>
+            <filmcarte :filmContent="film"> </filmcarte>
+            <br>
+        </div>
+    </div>
+</template>
+
+<script>
+
+const axios = require('axios').default;
+import filmcarte from '../components/filmcarte.vue'
+
+export default {
+
+    name:'HomePage',
+    components: {
+        filmcarte
+    },
+    data(){
+        return{
+            films: null,
+        }
+    },
+    mounted(){
+        axios
+            .get('http://www.omdbapi.com/?s=batman&apikey=904eed5c')
+            .then(response => (this.films = response.data.Search))
+    }
+}
+</script>
+
+<style>
+.centre{
+    width: 50%;
+    margin-left:auto;
+    margin-right:auto;
+}
+.liste{
+    text-align:left; 
+    
+}
+
+h1{
+    text-align:center;
+}
+</style>
